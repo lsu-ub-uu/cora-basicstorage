01daeee3a9497aa56c20bcd6c14cb734acc39581
madde <madde@642a36e7a83b>
2020-03-24 14:27:46 +0000

    Changed version of coradata and bascicdata

---
+++ src/test/java/se/uu/ub/cora/basicstorage/DataAttributeSpy.java
@@ -0,0 +1,23 @@
+package se.uu.ub.cora.basicstorage;
+
+import se.uu.ub.cora.data.DataAttribute;
+
+public class DataAttributeSpy implements DataAttribute {
+
+	public DataAttributeSpy(String id, String value) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getNameInData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
