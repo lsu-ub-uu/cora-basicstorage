49a4d755cc09da41c408e33a63b1997b31e054c3
pere <pere@5ff3e0758b40>
2023-05-12 13:51:36 +0000

    update dependencies

--- src/test/java/se/uu/ub/cora/basicstorage/DataAtomicSpy.java
+++ src/test/java/se/uu/ub/cora/basicstorage/DataAtomicSpy.java
@@ -1,9 +1,12 @@
 /**Copyright 2019 Uppsala University Library**This file is part of Cora.**Cora is free software:you can redistribute it and/or modify*it under the terms of the GNU General Public License as published by*the Free Software Foundation,either version 3 of the License,or*(at your option)any later version.**Cora is distributed in the hope that it will be useful,*but WITHOUT ANY WARRANTY;without even the implied warranty of*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the*GNU General Public License for more details.**You should have received a copy of the GNU General Public License*along with Cora.If not,see<http://www.gnu.org/licenses/>.
-*/package se.uu.ub.cora.basicstorage;
+*/
+package se.uu.ub.cora.basicstorage;
 
 import java.util.Collection;
+import java.util.Optional;
 
-import se.uu.ub.cora.data.DataAtomic;import se.uu.ub.cora.data.DataAttribute;
+import se.uu.ub.cora.data.DataAtomic;
+import se.uu.ub.cora.data.DataAttribute;
 
 public class DataAtomicSpy implements DataAtomic {
 	public String nameInData;
@@ -58,6 +61,12 @@ public class DataAtomicSpy implements DataAtomic {
 	public Collection<DataAttribute> getAttributes() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public Optional<String> getAttributeValue(String nameInData) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
 	}
 
 }
