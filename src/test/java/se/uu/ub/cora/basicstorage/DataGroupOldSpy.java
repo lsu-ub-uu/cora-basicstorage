e9273c8bcb9fbdcc56a04dea7e13beb7cc26014d
olovm <olov.mckie@ub.uu.se>
2025-09-19 12:17:16 +0000

    updated dependencies

--- src/test/java/se/uu/ub/cora/basicstorage/DataGroupOldSpy.java
+++ src/test/java/se/uu/ub/cora/basicstorage/DataGroupOldSpy.java
@@ -289,4 +289,10 @@ public class DataGroupOldSpy implements DataGroup {
 		return false;
 	}
 
+	@Override
+	public <T extends DataChild> List<T> getChildrenOfType(Class<T> type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
